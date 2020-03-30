15ad66c88c17cc458f336afa8b7582c394ebedc4
mswiczar <mswiczar@gmail.com>
2020-03-30 18:37:17 -0300

    Just Added Talle y Colore first proc

--- src/main/java/mav/Scrapping.java
+++ src/main/java/mav/Scrapping.java
@@ -13,7 +13,7 @@ public class Scrapping {
 	
 	public static final String url = "https://www.embrujojeans.com/marca-embrujo.html?&evento=&precioMenorReal=&precioMayorReal=&ofertas=&menu=&dato_a_buscar=&cate=&filtro_marca=&filtro_color=&filtro_talle=&filtro_categoria=&filtro_subcategoria=&filtro_tercercategoria=&aplicarPrecios=&mantenerPrecios=&page=";
 	public static final String base_url = "https://www.embrujojeans.com/";
-
+    public static final int maxIteraccion = 1;
 	public static ArrayList<HashMap<String,String> > listItems = new ArrayList<HashMap<String,String>>() ;
 
 	public static void  updateWithImagesPriceAndSize()
@@ -21,17 +21,71 @@ public class Scrapping {
 		
 		for (HashMap<String,String> elemento: listItems)
 		{
-			  System.out.println(elemento.get("SKU"));
+				System.out.print( "-------------------------------------------\n");
+			    System.out.println(elemento.get("SKU"));
+			  	Document document = getHtmlDocument(base_url+"/"+elemento.get("Ficha"));
+				
+				//talle y color
+				//gridcart-wrapp
+				//attribute -<div class="attribute">
+				//<input type="text" class="m-cantidad" data-codigo="9076/5-bl" data-color="#ffffff|Blanco" data-talle="Talle Único" data-stock="3" data-precio="310" placeholder="0">
+
+			  	Elements entradas = document.select("div.gridcart-wrapp");
+				System.out.println("Cantidad Talle y Color: "+entradas.size()+"\n");
+				
+				String Talles="";
+				String Colores="";
+				
+				for (Element elem : entradas) 
+				{
+					//talle
+					//color
+					String Talle = elem.getElementsByTag("input").attr("data-talle");
+					String Color = elem.getElementsByTag("input").attr("data-color");
+					if (Color.length()>0)
+					{
+				        String[] auxArray = Color.split("|");
+				        if (auxArray.length > 1)
+				        {
+							//myHashItem.put("id_external",auxArray[1]);
+				        }
+					}
+					if (Talles.length()==0)
+					{
+						Talles = Talles + Talle;
+					}
+					else
+					{
+						Talles = Talles + "," Talle;
+					}
+					elemento.put("Talles",Talles);
+					elemento.put("Colores",Colores);
+					
+					System.out.print("Talles: " + Talles + " | ");
+					System.out.print("Colores: "  + Colores+ " | ");
+					System.out.print("\n");
+					
+					
+				}
+				
+				
+			  
+			  
+				
+				//imagenes hasta 3 
+
+				//class b1
+				//t1
+				//<img src="productos/1574776834/250w_01_1583408990.jpg">
+			  	//entradas = document.select("div.d2.t1");
+				//System.out.println("Cantidad Images: "+entradas.size()+"\n");
+			  
+			  
+			  
 		}
-		//talle y color
-		//gridcart-wrapp
-		//attribute -<div class="attribute">
-		//<input type="text" class="m-cantidad" data-codigo="9076/5-bl" data-color="#ffffff|Blanco" data-talle="Talle Único" data-stock="3" data-precio="310" placeholder="0">
 		
-		//imagenes hasta 3 
-		//class b1
-		//t1
-		//<img src="productos/1574776834/250w_01_1583408990.jpg">
+		
+		
 	}
 	
 	
@@ -41,7 +95,7 @@ public class Scrapping {
 				HashMap<String,String >myHashItem;
 				if (getStatusConnectionCode(url) == 200) 
 				{
-					for (int i = 0; i<30;i++)
+					for (int i = 0; i<maxIteraccion;i++)
 					{
 						System.out.print( "->" +i+ "\n");
 						System.out.print( "-------------------------------------------\n");
@@ -52,7 +106,7 @@ public class Scrapping {
 						{
 							break;
 						}
-						// Paseo cada una de las entradas	
+
 						for (Element elem : entradas) 
 						{
 						
@@ -75,7 +129,6 @@ public class Scrapping {
 						        {
 									myHashItem.put("id_external",auxArray[1]);
 						        }
-								
 						        
 								
 								listItems.add(myHashItem);
